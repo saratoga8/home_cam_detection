7b34dfd8d2e995dcf5b930400fe9472ba53d111c
Daniel Haimov <saratoga8@gmail.com>
2020-09-17 15:00:05 +0300

    added telegram

--- src/ios/telegram.js
+++ src/ios/telegram.js
@@ -1,11 +1,40 @@
 const commands = require('../commands')
+const yaml = require('js-yaml')
+const fs = require('fs')
+const api = require('node-telegram-bot-api')
 
+const confPath = 'resources/io.yml'
+
+function token() {
+    const conf = yaml.safeLoad(fs.readFileSync(confPath, 'utf8'))
+    const telegram = conf.telegram
+    return (telegram !== undefined) ? telegram.token : undefined
+}
+
+
+function hello() {
+    console.log("hello")
+}
+
+let bot = null
+const bot_cmds = [ {txt: "hello", exec: () => { hello() }} ]
 
 exports.io = {
     out: {
         send: (str) => { }
     },
     in: {
-        receive: (emitter) => { }
+        receive: (emitter = null) => {
+            bot = bot ?? new api(token(), {polling: true})
+            bot.on('message', (msg) => {
+                console.log(msg)
+                const command = bot_cmds.find(cmd => cmd.txt === msg.text().toString().toLowerCase())
+                if(command === undefined)
+                    console.log("Unknown command")
+                else
+                    console.log(command.exec)
+            })
+            bot.on('close', () => {process.exit(0)})
+        }
     }
 }
--- test/io.js
+++ test/io.js
@@ -2,7 +2,7 @@ const chai = require('chai')
 const spies = require('chai-spies')
 const expect = chai.expect
 const assert = chai.assert
-
+const fs = require('fs')
 
 chai.use(spies)
 const controller = require('../src/controller')
@@ -13,7 +13,20 @@ const io = require('../src/ios/io')
 chai.spy.on(io.ios.CLI.out, ['send'])
 chai.spy.on(console, ['error'])
 
+const ioConf = { path: 'resources/io.yml', copyPath:  '/tmp/io.yml'}
+
+const {sleep} = require('sleep')
+
+
+const yaml = require('js-yaml')
+const api = require('node-telegram-bot-api')
+
+
+
+
 describe('IO', () => {
+
+
     context('CLI', () => {
         it('Output', () => {
             const emitter = new EventEmitter()
@@ -40,6 +53,33 @@ describe('IO', () => {
         it("There is no used IO in configure", () => {
             assert.isNull(io.loadFrom('test/resources/ios/invalid.yml'), "Function should return NULL")
             expect(console.error).to.have.been.called(2)
+        })
+    })
+
+    context('Telegram', () => {
+        afterEach('restore IO config', () => { fs.copyFileSync(ioConf.copyPath, ioConf.path) })
+        beforeEach('backup IO config', () => {
+            fs.copyFileSync(ioConf.path, ioConf.copyPath)
+            fs.copyFileSync('test/resources/ios/telegram.yml', ioConf.path)
+        })
+
+        it('Hello', () => {
+            const conf = yaml.safeLoad(fs.readFileSync(ioConf.path, 'utf8'))
+            const telegram = conf.telegram
+            const token = (telegram !== undefined) ? telegram.token : undefined
+
+
+            const bot = new api(token, {polling: true})
+            bot.on('message', (msg) => {
+                bot.sendMessage(msg.chat.id, "hi!")
+            })
+        }).timeout(12000)
+
+        it.skip('Output', () => {
+            const emitter = new EventEmitter()
+            controller.run(emitter, io.ios.CLI)
+            emitter.emit("command", {name: commands.stopMotion.command_name})
+            expect(io.ios.CLI.out.send).to.have.been.called.with("OK")
         })
     })
 })
