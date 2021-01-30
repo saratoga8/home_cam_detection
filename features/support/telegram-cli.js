c4e16884fca64e9df08eff85c8d209966095a9ba
Daniel Haimov <saratoga8@gmail.com>
2021-01-30 16:52:47 +0200

    added telegram-cli info

--- features/support/telegram-cli.js
+++ features/support/telegram-cli.js
@@ -12,12 +12,5 @@ exports.botName = botName
 exports.dialogList = () => chaiExec(`${telegramCliPath} -e "dialog_list"`)
 exports.path = () => chaiExec(`which ${telegramCliPath}`)
 exports.sendMsg = (msg) => {
-    // chai.request("https://api.telegram.org")
-    //     .post("/bot1453920552:AAFH08aBvY6hyF8UYVI4iunsoeAxZUQ3jy0/sendMessage")
-    //     .send({'chat_id': '1320889499', 'text': "hello"})
-    //     .end((err, resp) => {
-    //         assert.equal(resp.status, 200)
-    //     })
-    //execSync(`(echo "dialog_list"; sleep 1; echo "msg ${botName} ${msg}") | telegram-cli > /dev/null`)
     execSync(`telegram-cli -DRWC -e "msg ${botName} ${msg}" > /dev/null`)
 }
