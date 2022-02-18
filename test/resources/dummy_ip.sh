886f43a1e3609a2554cf24516e80bec42067ff31
Daniel Haimov <saratoga8@gmail.com>
2022-02-18 14:06:49 +0200

    fixed script

--- test/resources/dummy_ip.sh
+++ test/resources/dummy_ip.sh
@@ -1,21 +1,19 @@
 #!/usr/bin/env bash
 
-
-
-
 if [ "delete" = "$1" ]
 then
-  [[ -n `lsmod | grep dummy` ]] && rmmod dummy
+  [[ -n `ip addr | grep dummy` ]] && ip link delete dummy0
 fi
 
 if [ "create" = "$1" ]
 then
   IP=$2
 
-  modprobe -v dummy numdummies=2
-  [[ -z `lsmod | grep dummy` ]] && echo "The dummy interface not created" && exit 1
-  ip addr add $IP/24 dev dummy0
-  [[ -z `ifconfig -a | grep $IP` ]] && echo "The IP $IP has not set to the dummy interface" && exit 1
+  ip link add dummy0 type dummy
+  [[ -z `ip addr | grep dummy0` ]] && echo "The dummy interface not created" && exit 1
+  ip address add $IP/26 dev dummy0
+  [[ -z `ip addr | grep $IP` ]] && echo "The IP $IP has not set to the dummy interface" && exit 1
+  ip link set dummy0 up
   exit 0
 fi
 
