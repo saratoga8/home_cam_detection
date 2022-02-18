c600577b636cc621e5a081597a25fb8c2a89caf8
Daniel Haimov <saratoga8@gmail.com>
2022-02-18 14:24:15 +0200

    upgrade packages

--- test/resources/dummy_ip.sh
+++ test/resources/dummy_ip.sh
@@ -2,18 +2,18 @@
 
 if [ "delete" = "$1" ]
 then
-  [[ -n `ip addr | grep dummy` ]] && ip link delete dummy0
+  [[ -n `ip addr | grep dummy` ]] && sudo ip link delete dummy0
 fi
 
 if [ "create" = "$1" ]
 then
   IP=$2
 
-  ip link add dummy0 type dummy
+  sudo ip link add dummy0 type dummy
   [[ -z `ip addr | grep dummy0` ]] && echo "The dummy interface not created" && exit 1
-  ip address add $IP/26 dev dummy0
+  sudo ip address add $IP/26 dev dummy0
   [[ -z `ip addr | grep $IP` ]] && echo "The IP $IP has not set to the dummy interface" && exit 1
-  ip link set dummy0 up
+  sudo ip link set dummy0 up
   exit 0
 fi
 
